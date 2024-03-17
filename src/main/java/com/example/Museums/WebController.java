1ec4593de3834c59f44309f28fa6b51f399c022e
revoly afanasev <rewoly@mail.ru>
2024-03-18 00:21:22 +0900

    for student

--- src/main/java/com/example/Museums/WebController.java
+++ src/main/java/com/example/Museums/WebController.java
@@ -1,12 +1,43 @@
 package com.example.Museums;
 
+import com.example.Museums.hibernate.HibernateUtil;
+import com.example.Museums.hibernate.Museums;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс контроллер отвечает на запросы через веб - браузеры
+ */
 @Controller
 public class WebController {
+
     @GetMapping("/")
-    private String getMainPage() {
+    private String getMainPage(Model model) {
+        List<Museums> allMuseums = getMuseums();
+        model.addAttribute("museums", allMuseums);
         return "main";
     }
-}
+
+    /**
+     * Метод возвращает список музеев из БД
+     */
+    public synchronized List<Museums> getMuseums() {
+        Transaction transaction = null;
+        List<Museums> museums = new ArrayList<>();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            museums = session.createQuery("from Museums", Museums.class).list();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return museums;
+    }
+    }
