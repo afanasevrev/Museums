c785e7b2382fce28574d902094a0e1ce7d58725f
revoly afanasev <rewoly@mail.ru>
2024-03-17 23:12:38 +0900

    for student

---
+++ src/test/java/com/example/Museums/MuseumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Museums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
